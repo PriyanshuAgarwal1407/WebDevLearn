f63dd05aa45dc81cbaebbbcd668dac9d97c0d3ab
Priyanshu Agarwal <priyanshuagarwal1407@gmail.com>
2026-02-13 21:58:42 +0530

    Add question.js

---
+++ JavaScript/question.js
@@ -0,0 +1,22 @@
+// console.log(nm);  // undefined
+// var nm = "Priyanshu"; 
+
+
+// console.log(age);  // ReferenceError: Cannot access 'age' before initialization
+// let age = 25;
+
+
+// var x = 1;     // global
+// {
+//     var x = 2;    // global
+// }
+// console.log(x); // 2
+
+
+
+// let a = 10;      // block scope   // global
+// {
+//     let a = 20;
+//     console.log("Inside: ", a);     // Inside:  20
+// }
+// console.log("Outside :", a);        // Outside : 10
